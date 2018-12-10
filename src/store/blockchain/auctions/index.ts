8e040abaedb2ad4d513932c251374173471c9f9a
Sebastián Galiano <sgaliano@gmail.com>
2018-12-10 10:24:48 -0300

    Update running auction list every minute

--- src/store/blockchain/auctions/index.ts
+++ src/store/blockchain/auctions/index.ts
@@ -20,52 +20,63 @@ const reducer: Reducer<AuctionsState> = (state = {}, action) => {
   }
 };
 
+const RUNNING_AUCTION_INTERVAL = 1000 * 60; // 1 minute
+
 export function fetchRunningAuctions() {
   const { dx } = window;
 
+  let subscription: any;
+
   return async (dispatch: Dispatch, getState: () => AppState) => {
-    const state = getState();
-    const tokens = getAvailableTokens(state);
-    const tokenAddresses = Object.keys(tokens);
+    await checkForUpdates();
 
-    if (tokenAddresses.length) {
-      const tokenPairs = await dx.getRunningTokenPairs(tokenAddresses);
+    async function checkForUpdates() {
+      const state = getState();
+      const tokens = getAvailableTokens(state);
+      const tokenAddresses = Object.keys(tokens);
 
-      const runningAuctions = await Promise.all(
-        tokenPairs.map(
-          async ([t1, t2]): Promise<Auction> => {
-            const [auctionIndex, auctionStart, sellVolume, buyVolume] = await Promise.all([
-              dx.getLatestAuctionIndex(t1, t2),
-              dx.getAuctionStart(t1, t2),
-              dx.getSellVolume(t1, t2),
-              dx.getBuyVolume(t1, t2)
-            ]);
+      if (tokenAddresses.length) {
+        const tokenPairs = await dx.getRunningTokenPairs(tokenAddresses);
 
-            const currentPrice = await dx.getCurrentPrice(t1, t2, auctionIndex);
+        const runningAuctions = await Promise.all(
+          tokenPairs.map(
+            async ([t1, t2]): Promise<Auction> => {
+              const [auctionIndex, auctionStart, sellVolume, buyVolume] = await Promise.all([
+                dx.getLatestAuctionIndex(t1, t2),
+                dx.getAuctionStart(t1, t2),
+                dx.getSellVolume(t1, t2),
+                dx.getBuyVolume(t1, t2)
+              ]);
 
-            const sellTokenAddress = t1.toLowerCase();
-            const sellToken = tokens[sellTokenAddress];
-            const buyTokenAddress = t2.toLowerCase();
-            const buyToken = tokens[buyTokenAddress];
+              const currentPrice = await dx.getCurrentPrice(t1, t2, auctionIndex);
 
-            return {
-              auctionIndex,
-              sellToken: sellToken ? sellToken.symbol : '',
-              sellTokenAddress,
-              sellVolume,
-              buyToken: buyToken ? buyToken.symbol : '',
-              buyTokenAddress,
-              buyVolume,
-              auctionStart,
-              auctionEnd: '',
-              currentPrice,
-              state: 'running'
-            };
-          }
-        )
-      );
+              const sellTokenAddress = t1.toLowerCase();
+              const sellToken = tokens[sellTokenAddress];
+              const buyTokenAddress = t2.toLowerCase();
+              const buyToken = tokens[buyTokenAddress];
 
-      dispatch(setRunningAuctions(runningAuctions));
+              return {
+                auctionIndex,
+                sellToken: sellToken ? sellToken.symbol : '',
+                sellTokenAddress,
+                sellVolume,
+                buyToken: buyToken ? buyToken.symbol : '',
+                buyTokenAddress,
+                buyVolume,
+                auctionStart,
+                auctionEnd: '',
+                currentPrice,
+                state: 'running'
+              };
+            }
+          )
+        );
+
+        dispatch(setRunningAuctions(runningAuctions));
+      }
+
+      clearTimeout(subscription);
+      subscription = setTimeout(checkForUpdates, RUNNING_AUCTION_INTERVAL);
     }
   };
 }
