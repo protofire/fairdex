55cd586648c516a9bad1571b277b40b0b84ba4a7
Sebastián Galiano <sgaliano@gmail.com>
2018-12-11 09:59:35 -0300

    Show full value on hover over numeric field

--- src/components/formatters/Numeric.tsx
+++ src/components/formatters/Numeric.tsx
@@ -5,16 +5,20 @@ interface Props {
   value: number | string;
   decimals?: number;
   defaultValue?: string;
+  showTitle?: boolean;
 }
 
-const Numeric = ({ value, defaultValue, decimals = 18 }: Props) => {
-  const n = new BigNumber(value);
+const Numeric = ({ value, defaultValue, decimals = 18, showTitle }: Props) => {
+  const val = new BigNumber(value);
+  const raw = showTitle ? (val.isFinite() ? val.toString() : defaultValue) : '';
+  const formatted = val.isFinite() ? parseFloat(val.toFixed(decimals)) : defaultValue;
 
-  return <span>{n.isFinite() ? parseFloat(n.toFixed(decimals)) : defaultValue}</span>;
+  return <span title={raw}>{formatted}</span>;
 };
 
 Numeric.defaultProps = {
   defaultValue: '-',
+  showTitle: true,
 };
 
 export default Numeric;
