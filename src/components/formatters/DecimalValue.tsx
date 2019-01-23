8e08f80b307644217db7014acba52c9c5cfff4b7
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-01-23 12:56:30 -0300

    Idicate when numbers are being rounded to 0

--- src/components/formatters/DecimalValue.tsx
+++ src/components/formatters/DecimalValue.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
 import styled from 'styled-components';
-import { Decimal, DecimalFormat, formatNumber } from '../../contracts/utils';
+import { Decimal, DecimalFormat, formatNumber, toBigNumber } from '../../contracts/utils';
 
-interface Props extends DecimalFormat {
+interface Props extends DecimalFormat, HTMLAttributes<HTMLSpanElement> {
   value?: Decimal;
   defaultValue?: string;
   hideTitle?: boolean;
-  className?: string;
 }
 
-const DecimalValue = ({ className, value, defaultValue, decimals, hideTitle, prefix, postfix }: Props) => (
-  <Wrapper
-    className={className}
-    title={!hideTitle && value ? formatNumber(value, { prefix, postfix }) : undefined}
-  >
-    {value ? formatNumber(value, { decimals, prefix, postfix }) || defaultValue : ''}
-  </Wrapper>
-);
+const DecimalValue = ({ value, defaultValue, decimals, hideTitle, prefix, postfix, ...props }: Props) => {
+  let formatedValue = <>{value && formatNumber(value, { decimals, prefix, postfix })}</>;
+  if (value && toBigNumber(value).gt(0) && formatNumber(value, { decimals }) === '0') {
+    formatedValue = (
+      <>
+        {`${prefix ? prefix + ' ' : ''}0.0`}
+        <Elipsis />
+        {`${postfix ? ' ' + postfix : ''}`}
+      </>
+    );
+  }
+
+  return (
+    <Wrapper {...props} title={!hideTitle && value ? formatNumber(value, { prefix, postfix }) : undefined}>
+      {value ? formatedValue || defaultValue : ''}
+    </Wrapper>
+  );
+};
+
+const Elipsis = styled.span.attrs({
+  children: '…',
+})`
+  font-size: 50%;
+`;
 
 const Wrapper = styled.span`
   white-space: nowrap;
