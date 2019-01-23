d3bdbdf6dafdc6b25784ccc1b6d86b3dc4bd2829
Sebastián Galiano <sgaliano@gmail.com>
2019-01-23 09:33:43 -0300

    Fix decimal input

--- src/components/DecimalInput.tsx
+++ src/components/DecimalInput.tsx
@@ -1,53 +1,41 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { toBigNumber } from '../contracts/utils';
 import Input, { InputProps } from './Input';
 
-interface Props extends InputProps {
+interface DecimalInputProps extends InputProps {
   inputRef: React.Ref<HTMLInputElement>;
   onValueChange?: (value: BigNumber) => void;
 }
 
-interface State {
-  value: string;
-}
+const DecimalInput = React.memo(({ inputRef, onValueChange, ...props }: DecimalInputProps) => {
+  const [value, setValue] = useState(props.value ? props.value.toString() : '');
 
-class DecimalInput extends React.PureComponent<Props, State> {
-  state: State = { value: this.props.value ? this.props.value.toString() : '' };
+  useEffect(() => {
+    if (value !== props.value) {
+      const num = toBigNumber(value);
 
-  componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>) {
-    const value = this.props.value ? this.props.value.toString() : '';
-
-    if (value !== prevState.value) {
-      this.setState({ value });
+      if (onValueChange && num.toString(10) !== props.value) {
+        onValueChange(num);
+      }
     }
-  }
+  });
 
-  handleChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+  const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(event => {
     const inputValue = event.target.value ? event.target.value.trim() : '';
 
-    if (typeof this.props.onValueChange !== 'function') {
+    if (typeof onValueChange !== 'function') {
       return;
     }
 
     if (inputValue === '' || event.target.value.match(/^(\d+\.?\d*|\.\d+)$/)) {
-      this.setState({ value: inputValue || '0' });
-
-      const value = toBigNumber(inputValue);
-
-      if (value) {
-        this.props.onValueChange(value);
-      }
+      setValue(inputValue || '0');
     }
-  };
+  }, []);
 
-  render() {
-    const { inputRef, onValueChange, ...props } = this.props;
+  return <Input ref={inputRef} {...props} value={value} onChange={handleChange} />;
+});
 
-    return <Input ref={inputRef} {...props} value={this.state.value} onChange={this.handleChange} />;
-  }
-}
-
-export default React.forwardRef<HTMLInputElement, Omit<Props, 'inputRef'>>((props, ref) => (
+export default React.forwardRef<HTMLInputElement, Omit<DecimalInputProps, 'inputRef'>>((props, ref) => (
   <DecimalInput inputRef={ref} {...props} />
 ));
