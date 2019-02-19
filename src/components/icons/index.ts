f20cdd1e16e20c7aa5dd286fbb0ec840d81013cc
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-02-19 15:37:00 -0300

    Change wallet image base on user wallet selection

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,10 +2,12 @@ import { default as Back } from './Back';
 import { default as Close } from './Close';
 import { default as Menu } from './Menu';
 import { default as Preferences } from './Preferences';
+import { default as Wallet } from './Wallet';
 
 export default {
   Back,
   Close,
   Menu,
   Preferences,
+  Wallet,
 };
